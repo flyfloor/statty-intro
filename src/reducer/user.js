068e37bee0edf9b2db36802e852f94058b3887e5
jerryshew <jerryshew@gmail.com>
2017-10-10 19:11:37 +0800

    add comment

--- src/reducer/user.js
+++ src/reducer/user.js
@@ -18,6 +18,7 @@ const defaultUpdateUser = {
 
 export default (state, action) => {
     switch (action.type) {
+        // delete user
         case DELETE_USER:
             if (!action.user || !action.user.id) {
                 return state
@@ -25,19 +26,23 @@ export default (state, action) => {
             return Object.assign({}, state, {
                 users: state.users.filter(u => u.id !== action.user.id)
             })
+        // show create user form
         case SHOW_CREATE_USER:
             return Object.assign({}, state, {
                 showCreate: true,
                 updateUser: defaultUpdateUser
             })
+        // hide create user form
         case HIDE_CREATE_USER:
             return Object.assign({}, state, {
                 showCreate: false
             })
+        // update user form field value
         case UPDATE_USER_FORM:
             let { updateUser } = state
             let newUpdateUser = Object.assign({}, updateUser, action.user)
             return Object.assign({}, state, { updateUser: newUpdateUser })
+        // create user
         case CREATE_USER:
             let newUserState = {
                 users: state.users.concat(Object.assign({ id: uuidv1() }, state.updateUser)),
@@ -45,8 +50,10 @@ export default (state, action) => {
                 showCreate: false,
             }
             return Object.assign({}, state, newUserState)
+        // show edit user form
         case SHOW_EDIT_USER:
             return Object.assign({}, state, { updateUser: action.user, showCreate: true })
+        // update user
         case UPDATE_USER:
             let newUsers = state.users.map(item => {
                 return item.id === state.updateUser.id ? Object.assign({}, state.updateUser) : item
